d232ac3713e5e15d03267e437c4e349d7305462e
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-04-18 08:10:15 +0900

    add init

--- models/layers/yolox/models/yolo_head.py
+++ models/layers/yolox/models/yolo_head.py
@@ -178,6 +178,18 @@ class YOLOXHead(nn.Module):
             else:
                 print("Could not compile YOLOXHead because torch.compile is not available")
 
+    def initialize_biases(self, prior_prob):
+        for conv in self.cls_preds:
+            b = conv.bias.view(1, -1)
+            b.data.fill_(-math.log((1 - prior_prob) / prior_prob))
+            conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+
+        for conv in self.obj_preds:
+            b = conv.bias.view(1, -1)
+            b.data.fill_(-math.log((1 - prior_prob) / prior_prob))
+            conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+
+
     def forward(self, xin, labels=None):
         train_outputs = []
         inference_outputs = []
